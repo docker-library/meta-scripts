8b4e0f376ac25529a43e4593f43b563bb39c4dce
Tianon Gravi <admwiggin@gmail.com>
2025-01-23 11:09:07 -0800

    (re-)Implement parallelism in deploy
    This requires the input to be in the correct order (children first), but that was already an implied constraint.
    
    In my testing, this takes our `.test/test.sh --deploy` block testing deploy from ~5s down to ~2.5s.
    
    When we originally introduced this, it collided with some other aging infrastructure in a bad way, kicking over everything, and had to be reverted.  We've since changed that and this should be safe to re-introduce.

--- registry/push.go
+++ registry/push.go
@@ -74,17 +74,8 @@ func EnsureManifest(ctx context.Context, ref Reference, manifest json.RawMessage
 			errors.Is(err, ociregistry.ErrBlobUnknown) ||
 			(errors.As(err, &httpErr) && httpErr.StatusCode() >= 400 && httpErr.StatusCode() <= 499) {
 			// this probably means we need to push some child manifests and/or mount missing blobs (and then retry the manifest push)
-			var manifestChildren struct {
-				// *technically* this should be two separate structs chosen based on mediaType (https://github.com/opencontainers/distribution-spec/security/advisories/GHSA-mc8v-mgrf-8f4m), but that makes the code a lot more annoying when we're just collecting a list of potential children we need to copy over for the parent object to push successfully
-
-				// intentional subset of https://github.com/opencontainers/image-spec/blob/v1.1.0/specs-go/v1/index.go#L21 to minimize parsing
-				Manifests []ocispec.Descriptor `json:"manifests"`
-
-				// intentional subset of https://github.com/opencontainers/image-spec/blob/v1.1.0/specs-go/v1/manifest.go#L20 to minimize parsing
-				Config *ocispec.Descriptor  `json:"config"` // have to turn this into a pointer so we can recognize when it's not set easier / more correctly
-				Layers []ocispec.Descriptor `json:"layers"`
-			}
-			if err := json.Unmarshal(manifest, &manifestChildren); err != nil {
+			manifestChildren, err := ParseManifestChildren(manifest)
+			if err != nil {
 				return desc, fmt.Errorf("%s: failed parsing manifest JSON: %w", ref, err)
 			}
 
